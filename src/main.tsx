e7d7ad90874d9a9af75271e2c5ee7e2f58630319
kyleheiser-FL <kyleheiser@gmail.com>
2026-05-25 19:27:23 -0400

    Changed logos to get ready for push to production

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import {Analytics} from '@vercel/analytics/react';
 import App from './App.tsx';
 import './index.css';
 
@@ -20,7 +19,6 @@ if ('serviceWorker' in navigator && !window.location.hostname.includes('localhos
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <Analytics />
   </StrictMode>,
 );
 
