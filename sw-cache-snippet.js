7767014b711c33d0bbb48e1a4197879825b2f008
kyleheiser-FL <kyleheiser@gmail.com>
2026-05-25 21:52:36 -0400

    Add files via upload
    Perplexity files

---
+++ sw-cache-snippet.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "labrat-v2.7";
+const ASSETS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/labrat_icon.svg",
+  "/icon_72.png",
+  "/icon_96.png",
+  "/icon_128.png",
+  "/icon_144.png",
+  "/icon_152.png",
+  "/icon_167.png",
+  "/icon_180.png",
+  "/icon_192.png",
+  "/icon_256.png",
+  "/icon_384.png",
+  "/icon_512.png",
+  "/apple-touch-icon.png",
+  "/favicon-32.png",
+  "/favicon-16.png",
+  "/screenshot1.png",
+  "/screenshot_mobile.png"
+];
