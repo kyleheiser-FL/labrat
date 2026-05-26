bcf667ff60ba58df03c7fc066c82c244185504ec
kyleheiser-FL <kyleheiser@gmail.com>
2026-05-25 20:32:40 -0400

    Update sw.js
    Updating

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,5 @@
 // Minimal service worker to satisfy PWA installability requirements
+importScripts("https://progressier.app/8KBcaWU5kRnoxqhxaPba/sw.js" );
 const CACHE_NAME = 'labrat-v2.6';
 const ASSETS_TO_CACHE = [
   '/',
